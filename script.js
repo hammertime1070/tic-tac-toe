5e5b3f9edc53fe59a65faeaf3621525ac6d28e22
hammertime1070 <mbkeith614@gmail.com>
2023-09-14 09:44:44 -0400

    Add javascript class structure

---
+++ script.js
@@ -0,0 +1,37 @@
+const gameBoard = (function(){
+    // private variables and functions
+
+    // public variables and functions
+
+    return {
+        // declare public variables and functions
+    }
+})();
+
+
+const displayController = (function(){
+    // private variables and functions
+
+    // public variables and functions
+
+    return {
+        // declare public variables and functions
+    }
+})();
+
+
+const gameController = (function(){
+    // private variables and functions
+
+    // public variables and functions
+
+    return {
+        //declare public variables and functions
+    }
+})();
+
+const player = () => {
+    // class variables and functions
+    return{} // return all class variables and functions
+}
+
