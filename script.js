7177b873c7614cbd4dea656fb723f7c172bf46e3
hammertime1070 <mbkeith614@gmail.com>
2023-09-14 10:58:04 -0400

    Add board initialization and square factory

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const gameBoard = (function(){
     // private variables and functions
-
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    for (let i = 0; i < rows; i++) {
+        board[i] = []
+        for (let j = 0; j < columns; j++) {
+            board[i].push(square)
+        }
+    }
     // public variables and functions
 
     return {
         // declare public variables and functions
     }
 })();
+
+
+const square = () => {
+    // class variables and functions
+    const markSquare = (player) => {
+        value = player.mark
+    };
+
+    const getMark = () => value;
+
+    return{
+        markSquare,
+        getMark
+    } // return all class variables and functions
+}
 
 
 const displayController = (function(){
